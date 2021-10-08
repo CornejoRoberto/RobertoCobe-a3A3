522286770050e5c99bc4f7b507ce8296defecf7c
CobeñaCornejoRoberto <roberto_samp@outlook.com>
2021-10-08 17:37:30 -0500

    Agregar archivos de proyecto.

---
+++ RobertoCobeña3A3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RobertoCobeña3A3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0; double nume2 = 0; double suma = 0;
+                do{
+                Console.Write("Introduzca la cantidad a comprar:");
+                num1 = int.Parse(Console.ReadLine());
+                if (num1 >= 1){
+                Console.Write("Introduzca el precio del producto:");
+                nume2 = Double.Parse(Console.ReadLine());
+                if (nume2 >= 1)
+                suma = (num1 * nume2);
+                Console.WriteLine("Su valor total a pagar es de" + suma + "$ dolares");
+                Console.WriteLine("Si desea finalizar el proceso presione 0, en caso de querer continuar vuelva a introducir su cantidad deseada.");}
+            } while (num1 > 0);
+            Console.WriteLine("Gracias por su compra");
+        }
+    }
+}
